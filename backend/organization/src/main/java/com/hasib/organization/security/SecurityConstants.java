3a697570727079447870ecc050fbd6d1789ec9d8
S M Al Hasib <alhasibsm@gmail.com>
2024-05-26 08:23:10 +0600

    unknown changes - will not do anything now

--- backend/organization/src/main/java/com/hasib/organization/security/SecurityConstants.java
+++ backend/organization/src/main/java/com/hasib/organization/security/SecurityConstants.java
@@ -1,6 +1,8 @@
 package com.hasib.organization.security;
 
+import java.time.Duration;
+
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 86400000;
+    public static final long JWT_EXPIRATION = Duration.ofHours(1).toMillis();
     public static final String JWT_SECRET = "mita";
 }
